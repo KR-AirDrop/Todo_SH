684a604502513cad769f39b66be38454f102b10a
KR-AirDrop <choish4682@naver.com>
2022-02-26 11:06:25 +0900

    8080포트를 열고, 문장 출력~

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.listen();
+// listen (파라미터1, 파라미터2), 파라미터1 : 서버 띄울 포트번호, 파라미터2 : 띄운 후 실행할 코드
+app.listen(8080, function () {
+  console.log("listening on 8080");
+});
