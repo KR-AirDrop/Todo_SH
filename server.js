f2b9ce0c513e3da0dc9a70d10765363e07bc3ea3
KR-AirDrop <choish4682@naver.com>
2022-03-03 22:37:43 +0900

    로그인 구현...?

--- server.js
+++ server.js
@@ -87,7 +87,6 @@ app.get("/detail/:id", (req, res) => {
   db.collection("post").findOne(
     { _id: parseInt(req.params.id) },
     (err, result) => {
-      console.log(result);
       if (!result) {
         res.render(__dirname + "/views/error.ejs");
       } else {
@@ -105,4 +104,80 @@ app.put("/edit", function (req, res) {
       res.redirect("/list");
     }
   );
+});
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+
+app.use(
+  session({ secret: "seonchoi", resave: true, saveUninitialized: false })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/login", function (req, res) {
+  res.render("login.ejs");
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/fail",
+  }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+app.get("/mypage", logined, function (req, res) {
+  console.log(req.user);
+  res.render("mypage.ejs", { user: req.user });
+});
+
+function logined(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.render("login.ejs");
+  }
+}
+
+// 아이디 비번 인증하는 세부 코드
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "id",
+      passwordField: "pw",
+      session: true,
+      passReqToCallback: false,
+    },
+    function (inputID, inputPW, done) {
+      db.collection("login").findOne({ id: inputID }, function (err, result) {
+        if (err) return done(err);
+
+        if (!result)
+          return done(null, false, {
+            message: "존재하지 않는 아이디입니다.",
+          });
+        if (inputPW == result.pw) {
+          return done(null, result);
+        } else {
+          return done(null, false, { message: "비밀번호가 틀렸습니다." });
+        }
+      });
+    }
+  )
+);
+
+// 세션 저장
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+// 이 세션 데이터 가진 사람을 DB에서 찾기
+passport.deserializeUser(function (아이디, done) {
+  db.collection("login").findOne({ id: 아이디 }, function (err, result) {
+    done(null, result);
+  });
 });
