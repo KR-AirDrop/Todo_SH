d888f5f057f67f8f78ed293171ad005974e9c618
KR-AirDrop <choish4682@naver.com>
2022-03-03 21:17:35 +0900

    edit기능 마무리, 각 링크 연결

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const res = require("express/lib/response");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 const MongoClient = require("mongodb").MongoClient;
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
+app.use("/public", express.static("public"));
 
 var db;
 MongoClient.connect(
@@ -21,30 +24,41 @@ MongoClient.connect(
 
 // get ('경로', function(요청, 응답){} )
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index.ejs");
 });
 
 app.get("/write", (req, res) => {
-  res.sendFile(__dirname + "/write.html");
+  res.render("write.ejs");
+});
+
+app.get("/edit/:id", (req, res) => {
+  db.collection("post").findOne(
+    { _id: parseInt(req.params.id) },
+    function (err, result) {
+      if (!result) {
+        res.render("error.ejs");
+      } else {
+        res.render("edit.ejs", { post: result });
+      }
+    }
+  );
 });
 
 // 어떤 사람이 /add경로로 POST요청을 하면 ... ~를 실행해주세요.
 app.post("/add", (req, res) => {
-  res.send("전송완료");
-
   db.collection("counter").findOne({ name: "게시물 갯수" }, (err, result) => {
     var totalPost = result.totalPost;
     // DB에 저장!
     db.collection("post").insertOne(
       { _id: totalPost + 1, todo: req.body.todo, date: req.body.date },
       (err, result) => {
-        console.log("저장완료");
         // 총 게시물 갯수 +1 해줘야함
         db.collection("counter").updateOne(
           { name: "게시물 갯수" },
           { $inc: { totalPost: 1 } },
           (err, result) => {
             if (err) return console.log(err);
+            else res.redirect("/list");
           }
         );
       }
@@ -75,10 +89,20 @@ app.get("/detail/:id", (req, res) => {
     (err, result) => {
       console.log(result);
       if (!result) {
-        res.sendFile(__dirname + "/error.html");
+        res.render(__dirname + "/views/error.ejs");
       } else {
         res.render("detail.ejs", { data: result });
       }
+    }
+  );
+});
+
+app.put("/edit", function (req, res) {
+  db.collection("post").updateOne(
+    { _id: parseInt(req.body.id) },
+    { $set: { todo: req.body.todo, date: req.body.date } },
+    function (err, result) {
+      res.redirect("/list");
     }
   );
 });
