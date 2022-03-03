135cb810ef8bf5e8387ca7015dce673495ef4262
KR-AirDrop <choish4682@naver.com>
2022-03-03 14:41:35 +0900

    mongoDB 데이터 저장해보기

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 const MongoClient = require("mongodb").MongoClient;
+
+var db;
 MongoClient.connect(
-  "mongodb+srv://hyuktodo:chltjdgur1@cluster0.g0bua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  "mongodb+srv://hyuktodo:chltjdgur1@cluster0.g0bua.mongodb.net/hyuktodo?retryWrites=true&w=majority",
   (error, client) => {
+    if (error) return console.log(error);
+
+    db = client.db("hyuktodo");
+
+    db.collection("post").insertOne({ 이름: "성혁", 나이: 29 }, (err, 결과) => {
+      console.log("저장완료");
+    });
+
     app.listen(8080, () => {
       console.log("listening on 8080");
     });
